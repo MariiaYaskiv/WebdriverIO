826fcd230f77a3bd8e395605bd509cf92edcc058
Mariya <lytvyn.mariia@gmail.com>
2022-08-21 17:28:15 +0300

    added all tests with junit to run them with jenkins

--- test/wdio.conf.ts
+++ test/wdio.conf.ts
@@ -52,7 +52,7 @@ export const config: Options.Testrunner = {
   // then the current working directory is where your `package.json` resides, so `wdio`
   // will be called from there.
   //
-  specs: ["./test/specs/**/contextMenu.ts"],
+  specs: ["./test/specs/**/*.ts"],
   // Patterns to exclude.
   exclude: [
     // 'path/to/excluded/files'
